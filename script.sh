18874c315fa8fc445b29d9648524254ab07abf8b
Vjs Pranav <pranavasri@live.in>
2018-08-29 00:10:56 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd packages/apps/Settings
+git remote add new https://github.com/garlic-n/android_packages_apps_Settings.git
+git fetch new 
+git cherry-pick bb33146f1641d7ef7b2897d7a6448c390fe76019
+cd ../../..
+
+echo thnx louis
