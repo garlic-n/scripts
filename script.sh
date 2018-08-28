2ee6d8d8d91f9ef900bf4f6125009bf44ac44b91
Vjs Pranav <pranavasri@live.in>
2018-08-29 00:15:47 +0530

    changes to script

--- script.sh
+++ script.sh
@@ -4,5 +4,6 @@ git remote add new https://github.com/garlic-n/android_packages_apps_Settings.gi
 git fetch new 
 git cherry-pick bb33146f1641d7ef7b2897d7a6448c390fe76019
 cd ../../..
+cp -r ../scr* script_build.sh
 
 echo thnx louis
